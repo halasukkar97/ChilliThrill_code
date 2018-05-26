d5298a0b127a78ae19bb25846e4a6b9ace77f0ab
hela sukkar <32265046+halasukkar97@users.noreply.github.com>
2018-05-26 03:14:56 +0100

    Add files via upload

---
+++ scripts/HighScore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+    private Scores score;
+
+    private SaveAndLoad saveandload;
+    public TMP_Text High_Score;
+    public TMP_Text Most_Coinst;
+    public TMP_Text Total_Games;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        if (score == null) score = GameObject.FindObjectOfType<Scores>();
+        if (saveandload == null) saveandload = GameObject.FindObjectOfType<SaveAndLoad>();
+        SaveAndLoad.Load();
+
+    }
+
+    private void Update()
+    {
+        //show the high scores
+        High_Score.text = "" + Mathf.Round(Scores.Highscore);
+        Most_Coinst.text = "" + Scores.MostCoins;
+        Total_Games.text = "" + Scores.TotalGames;
+    }
+
+
+}
---
+++ scripts/Water.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Water : MonoBehaviour {
+
+    private GameObject playerRef;
+    private Vector3 WaterYpos;
+    private float PlayerYpos;
+    
+    public void Start()
+    {
+        //start with saving the player positions
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        PlayerYpos = playerRef.transform.position.y;
+
+    }
+
+
+    public  void FixedUpdate()
+    {
+        //make the water follows the player where ever he goes but when he jumps calls the position y that has been saved in the start
+      WaterYpos = new Vector3(playerRef.transform.position.x, PlayerYpos, playerRef.transform.position.z);
+      this.transform.position = WaterYpos + new Vector3(-61f, -3.5f, -41.72f);
+
+    }
+
+}
